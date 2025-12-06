6d5805aad643a44035e27413aa01e4fa114971cd
fidanst-azmpa201-dev <fidanst-azmpa201@code.edu.az>
2025-12-06 14:54:10 +0400

    Fetch atildi,home sehifesi demek olar hazir,js qalib

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,24 +1,44 @@
 const response = await fetch("https://api.tvmaze.com/shows?&select=key1,key2,key3");
 const data = await response.json();
 
-const cardsDiv = document.querySelector(".cards");
+const cardsDiv = document.querySelector("#imgs");
 data.forEach(films => {
     cardsDiv.innerHTML += `
-     <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">${films.name}</h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">${films.type}</h6>
-                        <p class="card-text">${films.genres}</p>
-                        <a href="${films.url}" class="card-link">Film Linki</a>
-                        <a href="${films.officialSite}" class="card-link">Sayt Linki</a>
+     <div class="row">
+                <div class="img" style="width: 18rem;">
+                    <div class="images">
+                        <img src="${films.image.medium}"</img>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">${films.name}</h6>
+                        
+                        
                     </div>
                 </div>
             </div>
-  
-
 
     `
 });
 
 console.log(data);
+
+
+// const namesDiv=document.querySelector("#names");
+// data.forEach(filmss => {
+//     cardsDiv.innerHTML +=`
+//     <div class="row">
+//                 <div class="nms" style="width: 18rem;">
+//                     <div class="names">
+//                         
+                        
+//                     </div>
+//                 </div>
+//             </div>
+//     `
+// });
+
+// <h3>${films.name}</h5>
+                        // 
+                        // <p class="card-text">${films.genres}</p>
+                        // 
+                        // <a href="${films.officialSite}" class="card-link">Sayt Linki</a>
+
+
