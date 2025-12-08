978606835fd7a2ba97b219440e78947ef1c6bd86
fidanst-azmpa201-dev <fidanst-azmpa201@code.edu.az>
2025-12-08 07:04:52 +0400

    Tam olmasada bitdi

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,16 +2,16 @@ const response = await fetch("https://api.tvmaze.com/shows");
 const data = await response.json();
 
 const dataDiv1 = document.querySelector("#imgs");
-data.slice(0,40).forEach(films => {
+data.slice(0,10).forEach(films => {
     dataDiv1.innerHTML += `
-     <div class="row">
+     <div class="row" data-id="${films.id}">
                 <div class="img" style="width: 18rem;">
                     <div class="images">
                         <h4 class="card-subtitle mb-2 text-body-secondary">${films.name}</h4>
                         <img src="${films.image.medium}"</img>
                         <h5 class="card-subtitle mb-2 text-body-secondary">${films.language}</h5>
                         <p class="card-text">${films.genres}</p>
-                        
+                         <p class="card-text">${films.summary}</p>
                         
                     </div>
                 </div>
@@ -21,16 +21,16 @@ data.slice(0,40).forEach(films => {
 });
 
 const dataDiv2 = document.querySelector("#imgs2");
-data.slice(40,80).forEach(films => {
+data.slice(10,20).forEach(films => {
     dataDiv2.innerHTML += `
-     <div class="row">
+     <div class="row" data-id="${films.id}">
                 <div class="img" style="width: 18rem;">
                     <div class="images">
                         <h4 class="card-subtitle mb-2 text-body-secondary">${films.name}</h4>
                         <img src="${films.image.medium}"</img>
                         <h5 class="card-subtitle mb-2 text-body-secondary">${films.language}</h5>
                         <p class="card-text">${films.genres}</p>
-                        
+                         <p class="card-text">${films.summary}</p>
                         
                     </div>
                 </div>
@@ -40,15 +40,16 @@ data.slice(40,80).forEach(films => {
 });
 
 const dataDiv3 = document.querySelector("#imgs3");
-data.slice(80,120).forEach(films => {
+data.slice(20,30).forEach(films => {
     dataDiv3.innerHTML += `
-     <div class="row">
+     <div class="row  data-id="${films.id}"">
                 <div class="img" style="width: 18rem;">
                     <div class="images">
                         <h4 class="card-subtitle mb-2 text-body-secondary">${films.name}</h4>
                         <img src="${films.image.medium}"</img>
                         <h5 class="card-subtitle mb-2 text-body-secondary">${films.language}</h5>
                         <p class="card-text">${films.genres}</p>
+                         <p class="card-text">${films.summary}</p>
                         
                         
                     </div>
@@ -59,15 +60,16 @@ data.slice(80,120).forEach(films => {
 });
 
 const dataDiv4=document.querySelector("#imgs4");
-data.slice(120,160).forEach(films => {
+data.slice(30,40).forEach(films => {
     dataDiv4.innerHTML += `
-     <div class="row">
+     <div class="row  data-id="${films.id}"">
                 <div class="img" style="width: 18rem;">
                     <div class="images">
                         <h4 class="card-subtitle mb-2 text-body-secondary">${films.name}</h4>
                         <img src="${films.image.medium}"</img>
                         <h5 class="card-subtitle mb-2 text-body-secondary">${films.language}</h5>
                         <p class="card-text">${films.genres}</p>
+                        <p class="card-text">${films.summary}</p>
                     </div>
                 </div>
             </div>
@@ -88,17 +90,46 @@ arrows.forEach((arrow, i) => {
             movieLists[i].computedStyleMap().get("transform")[0].x.value
             - 270}px)`;
     });
-
-
 });
 
 
+const moviesContainer = document.getElementById("imgs4");
+const loadMoreBtn = document.getElementById("loadMoreBtn");
+
+let page = 1;
+const limit = 8;
+
+async function loadMovies() {
+    try {
+        const res = await fetch(`https://api.tvmaze.com/shows?page=${page}`);
+        const data = await res.json();
+
+        const sliced = data.slice(0, limit);
+
+        sliced.forEach(films => {
+            moviesContainer.innerHTML += `
+
+             <div class="images">
+                        <h4 class="card-subtitle mb-2 text-body-secondary">${films.name}</h4>
+                        <img src="${films.image.medium}"</img>
+                        <h5 class="card-subtitle mb-2 text-body-secondary">${films.language}</h5>
+                        <p class="card-text">${films.genres}</p>
+                        <p class="card-text">${films.summary}</p>
+                
+            `;
+        });
+
+        page++;
+
+    } catch (err) {
+        console.log("Error:", err);
+    }
+}
+
+loadMoreBtn.addEventListener("click", loadMovies);
+loadMovies();
 
 
-
-
-
-//<a href="${films.officialSite}" class="card-link">Click and watch</a>
 
 
 
